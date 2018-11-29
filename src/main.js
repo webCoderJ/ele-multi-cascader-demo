030b63f465e5af8a1162c5bf58d9cb3900e8c450
彭杰 <pengjie5@ZBMAC-C02VX14VH.local>
2018-11-29 17:34:41 +0800

    INIT

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Element from "element-ui"
+import eleMultiCascader from "ele-multi-cascader";
 
 Vue.config.productionTip = false
 
+Vue.use(Element)
+Vue.use(eleMultiCascader)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
