6d6810872c6d6832583123bae19e344962cb6e03
pengjie5 <pengjie5@jd.com>
2019-03-25 14:54:30 +0800

    MOD

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import Element from "element-ui"
 // import eleMultiCascader from "./components/ele-multi-cascader";
 import eleMultiCascader from "ele-multi-cascader";
+import GithubButton from 'vue-github-button';
 
 Vue.config.productionTip = false
 
 Vue.use(Element)
 Vue.use(eleMultiCascader)
+Vue.component(GithubButton.name, GithubButton)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
